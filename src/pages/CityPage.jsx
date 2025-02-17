5c31eb07a1c2c78645f6f94bf1aefb85c7f078c4
UlianaAsadulina <u.asadulina@icloud.com>
2025-02-17 13:11:30 -0500

    weather icons from react libraries

--- src/pages/CityPage.jsx
+++ src/pages/CityPage.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-
+import { WiThermometer, WiHumidity, WiStrongWind, WiFog } from "react-icons/wi";
+import { WiDaySunny, WiCloud, WiRain, WiSnow, WiStormShowers } from "react-icons/wi";
 
 
 export default function CityPage() {
@@ -8,7 +9,35 @@ export default function CityPage() {
     const param = useParams();
     const city = param.city;
 
-    console.log("Param:" + city);
+    // console.log("Param:" + city);
+
+
+    //function to get icon from react libraries based on weather description
+    const getWeatherIcon = (description) => {
+        switch (description.toLowerCase()) {
+            case "clear sky":
+                return <WiDaySunny size={60} />;
+            case "few clouds":
+                return <WiDayCloudy size={60} />;
+            case "scattered clouds":
+                return <WiDayCloudyHigh size={60} />;
+            case "broken clouds":
+                return <WiCloud size={60} />;
+            case "shower rain":
+                return <WiShowers size={60} />;
+            case "rain":
+                return <WiRain size={60} />;
+            case "thunderstorm":
+                return <WiStormShowers size={60} />;
+            case "snow":
+                return <WiSnow size={60} />;
+            case "mist":
+            case "fog":
+                return <WiFog size={60} />;
+            default:
+                return <WiCloud size={60} />; // Default to cloud icon
+        }
+    };
 
 
     useEffect(() => {
@@ -38,31 +67,32 @@ export default function CityPage() {
             <div className="weather-container">
                 <h1>{weather.name}, {weather.sys.country}</h1>
                 <h1>{Math.round(weather.main.temp)}°C</h1>
+                <div>{getWeatherIcon(weather.weather[0].description)}</div>
                 <h3>{weather.weather[0].description}</h3>
-                <h3></h3>
-                <h3>H: {Math.round(weather.main.temp_max)}°   L: {Math.round(weather.main.temp_min)}°</h3>
+                <h3>H: {Math.round(weather.main.temp_max)}° L: {Math.round(weather.main.temp_min)}°</h3>
+            
                 <div className="weather-details">
                     <div className="weather-box">
+                        <WiThermometer size={30} />
                         <p>FEELS LIKE</p>
                         <p>{Math.round(weather.main.feels_like)}°</p>
                     </div>
                     <div className="weather-box">
+                        <WiHumidity size={30} />
                         <p>HUMIDITY</p>
                         <p>{weather.main.humidity}%</p>
                     </div> 
                     <div className="weather-box">
+                        <WiStrongWind size={30} />
                         <p>WIND</p>
                         <p>{weather.wind.speed} m/s</p>
                     </div>   
                     <div className="weather-box">
+                        <WiFog size={30} />
                         <p>VISIBILITY</p>
                         <p>{weather.visibility}</p>
                     </div> 
                 </div>
-              
-              
-                
-                
             </div>
 
         ) : (
